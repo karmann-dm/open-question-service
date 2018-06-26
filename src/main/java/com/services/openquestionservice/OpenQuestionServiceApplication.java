6dbe0e432349c36d0796fd928459a4a47eca46ec
Karmanov Dmitry <karmanno@gmail.com>
2018-06-26 19:06:29 +0800

    Initial.

---
+++ src/main/java/com/services/openquestionservice/OpenQuestionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.services.openquestionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenQuestionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenQuestionServiceApplication.class, args);
+    }
+}
