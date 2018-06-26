61f55cde1562878f677c5c64a2cbe149ae6cf3ed
Karmanov Dmitry <karmanno@gmail.com>
2018-06-26 21:15:57 +0800

    Created empty OpenQuestion class.

---
+++ src/main/java/com/services/openquestionservice/service/model/OpenQuestion.java
@@ -0,0 +1,11 @@
+package com.services.openquestionservice.service.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "open_questions")
+public class OpenQuestion {
+
+}
