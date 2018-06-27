969d8851cdc850a50c7340d2c537cf322c4f041d
Karmanov Dmitry <karmanno@gmail.com>
2018-06-27 14:48:13 +1000

    Added @Repository annotation to questions repository.

--- src/main/java/com/services/openquestionservice/questionservice/repository/OpenQuestionRepository.java
+++ src/main/java/com/services/openquestionservice/questionservice/repository/OpenQuestionRepository.java
@@ -2,6 +2,8 @@ package com.services.openquestionservice.questionservice.repository;
 
 import com.services.openquestionservice.questionservice.model.OpenQuestion;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OpenQuestionRepository extends CrudRepository<OpenQuestion, Long> {
 }
