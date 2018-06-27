1b90f3bcd679df7944c95d7dee6df93834473fd6
Karmanov Dmitry <karmanno@gmail.com>
2018-06-27 14:47:51 +1000

    Added Repository annotation import.

--- src/main/java/com/services/openquestionservice/questionservice/repository/AnswerRepository.java
+++ src/main/java/com/services/openquestionservice/questionservice/repository/AnswerRepository.java
@@ -3,6 +3,7 @@ package com.services.openquestionservice.questionservice.repository;
 import com.services.openquestionservice.questionservice.model.Answer;
 import com.services.openquestionservice.questionservice.model.OpenQuestion;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
