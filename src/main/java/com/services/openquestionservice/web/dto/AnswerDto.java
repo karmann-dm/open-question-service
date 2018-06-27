a8197023042a6f0a9f0c8ec52fcbae9bcc7101a3
Karmanov Dmitry <karmanno@gmail.com>
2018-06-27 13:07:45 +1000

    Created simple DTO classes.

---
+++ src/main/java/com/services/openquestionservice/web/dto/AnswerDto.java
@@ -0,0 +1,49 @@
+package com.services.openquestionservice.web.dto;
+
+public class AnswerDto {
+    private Long openQuestionId;
+    private Long participantId;
+    private String answerText;
+    private Integer votes;
+
+    public AnswerDto() {
+    }
+
+    public AnswerDto(Long openQuestionId, Long participantId, String answerText, int votes) {
+        this.openQuestionId = openQuestionId;
+        this.participantId = participantId;
+        this.answerText = answerText;
+        this.votes = votes;
+    }
+
+    public AnswerDto(Long openQuestionId, Long participantId, String answerText) {
+        this.openQuestionId = openQuestionId;
+        this.participantId = participantId;
+        this.answerText = answerText;
+        this.votes = 0;
+    }
+
+    public Long getOpenQuestionId() {
+        return openQuestionId;
+    }
+
+    public void setOpenQuestionId(Long openQuestionId) {
+        this.openQuestionId = openQuestionId;
+    }
+
+    public Long getParticipantId() {
+        return participantId;
+    }
+
+    public void setParticipantId(Long participantId) {
+        this.participantId = participantId;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
+}
